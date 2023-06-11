088e5d41be0b00016429b712fb549d70f5842e87
max <max.lillie95@gmail.com>
2023-06-11 15:56:03 -0500

    routes set up for aquariums/fishes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #routes for fishes
+  get "/fishes" => "fish#index"
+  get "/fishes/:id" => "fish#show"
+  post "/fishes" => "fish#create"
+  patch "/fishes/:id" => "fish#update"
+  delete "/fishes/:id" => "fish#destroy"
+
+  #routes for aquariums
+  get "/aquariums" => "aquarium#index"
+  get "/aquariums/:id" => "aquarium#show"
+  post "/aquariums" => "aquarium#create"
+  patch "/aquariums/:id" => "aquarium#update"
+  delete "/aquariums/:id" => "aquarium#destroy"
 end
